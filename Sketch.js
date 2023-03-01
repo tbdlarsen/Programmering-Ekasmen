6fb4003f97d4d1fb08c7cd891b7846b6728e7fd7
tbdlarsen <105425030+tbdlarsen@users.noreply.github.com>
2023-03-01 09:06:39 +0100

    ændret

--- Sketch.js
+++ Sketch.js
@@ -10,7 +10,7 @@ function setup() {
 
     
     stroke(54);
-    for (let i = 1; i >= 8; i++){
+    for (let i = 1; i <= 8; i++){
         
          line((width/8)*i,0,(width/8)*i, height);
      
