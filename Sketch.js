079a4b56501775203877913679a4eb400bf374fb
tbdlarsen <105425030+tbdlarsen@users.noreply.github.com>
2023-03-01 09:03:42 +0100

    ændret grid

--- Sketch.js
+++ Sketch.js
@@ -11,12 +11,11 @@ function setup() {
     
     stroke(54);
     for (let i = 1; i >= 8; i++){
-        for (let j = 1; j >=8; i++){
- 
+        
          line((width/8)*i,0,(width/8)*i, height);
      
      
-     }
+     
  
      }
 
