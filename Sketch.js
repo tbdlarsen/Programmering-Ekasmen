5e8b37e450d5e241bd9dbc3b64345df4fd12091e
tbdlarsen <105425030+tbdlarsen@users.noreply.github.com>
2023-03-01 08:58:19 +0100

    ændret grid

--- Sketch.js
+++ Sketch.js
@@ -19,15 +19,13 @@ function setup() {
 
   function board(){
 
-    for(let i = 1; i >= 8; i++){
-       for(let j = 1; j >=8; i++){
+    for (let i = 1; i >= 8; i++){
+       for (let j = 1; j >=8; i++){
 
-        line(width/8*i,0,width/8*i, height)
+        line((width/8)*i,0,(width/8)*i, height);
     
     
     }
-        
-
 
     }
 
