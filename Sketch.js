03f4b0e0aba8d16763db1301c100efefe1590b5f
PhilipHellerup <114650179+PhilipHellerup@users.noreply.github.com>
2023-03-14 10:29:10 +0100

    Hej.

--- Sketch.js
+++ Sketch.js
@@ -1,34 +1,39 @@
+//Globale variabler
 let pawn;
-
 let positionX;
 let positionY;
+let spritesheet;
 
+//Preloader spritesheet så det klar til brug.
+function preload() {
+  spritesheet = loadImage("Image/Spritesheet.png");
+}
+
+
+//Funktion setup (køre kode en gang)
 function setup() {
-    createCanvas(800, 800);
-    pawn = new Pawn(120,120);
-    
-    
+  createCanvas(800, 800);
+  //Skaber en ny bonde
+  pawn = new Pawn(120,120);  
   }
-  
+
+//Funktion draw (køre kode i et loop)  
   function draw() {
     background(220);
-    
-    
     stroke(54);
-    for (let i = 1; i <= 7; i++){
-        
-         line((width/8)*i,0,(width/8)*i, height);
-         line(0,(height/8)*i,width,(height/8)*i)
- 
-     }
 
-     pawn.visual();
-     
+    //Indsætter klassefunktioner
+    pawn.visual();
+
+    //Laver spillebræt med linjer
+    for (let i = 1; i <= 7; i++){
+      line((width/8)*i,0,(width/8)*i, height);
+      line(0,(height/8)*i,width,(height/8)*i)
+     }
+          
+     //Finder musens koordinater og printer det til console
      positionX = int(map(mouseX,0,width,0,8));
      positionY = int(map(mouseY,0,height,0,8));
-
-
-
      print(positionX + " " + positionY)
      
 
@@ -40,9 +45,4 @@ function setup() {
 
 
 
-  function preload() {
-    spritesheet = loadImage("Spritesheet.png");
-    
-
-
-}
+  
