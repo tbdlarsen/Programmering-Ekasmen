d6414e0120f4ab0df04eab834ddbca94b9bdabb2
tbdlarsen <105425030+tbdlarsen@users.noreply.github.com>
2023-03-14 08:21:49 +0100

    ændret

--- Pawn.js
+++ Pawn.js
@@ -1,22 +1,68 @@
-let x;
-let y;
+
  
 class Pawn {
     constructor(x,y) {
-        this.x = 200;
-        this.y = 200;
+        this.x = x;
+        this.y = y;
+        /*this.spriteArray = [];
+        this.spritesheet;
+        */
+        
+
+
+    }
+
+    visual(){
+        square(this.x,this.y,50);
+        
+
+
+
+
     }
 
 
-    
+
+
+
+
+
+    /*
+   preload() {
+        spritesheet = loadImage("Image/Spritesheet.png");
+        
+
+
+    }
+
+    image() {
+        this.widthSpritesheet = spritesheet.width/6;
+        this.heightSpritesheet= spritesheet.height/2;
+        
+        for(let y = 0; y < 2; y++) {
+            spriteArray[y] = [];
+            
+            for (let x = 0; x < 6; x++) {
+                this.spriteArray[y][x] = this.spritesheet.get(x*this.widthSpritesheet,y*this.heightSpritesheet,this.widthSpritesheet,this,heightSpritesheet);
+            }
+        }
+
+
+
+    }
+
+    visual() {
+        image(spritesheet,200,200);
+
+    }
 
     
 
+*/
 
 
 
 
 
 
-
-} 
+}
