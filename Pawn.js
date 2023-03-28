94b5c643f1fdabc2092d0a02dc84e33314c6a6e0
tbdlarsen <105425030+tbdlarsen@users.noreply.github.com>
2023-03-28 13:51:13 +0200

    lavet om i pawn

--- Pawn.js
+++ Pawn.js
@@ -4,15 +4,15 @@ class Pawn {
         this.x = x;
         this.y = y;
         this.spritesheet;
-        this.billedX = x;
-        this.billedY = y;
+       
+       
         this.billedWidth = 640;
         this.billedHeight = 213;
     }
 
     //Visual funktion som viser en del af spritesheetet. 
     visual(color, piece) {    
-        image(spritesheet,this.billedX,this.billedY,this.billedWidth/6,this.billedHeight/2,
+        image(spritesheet,this.x,this.y,this.billedWidth/6,this.billedHeight/2,
         piece*(this.billedWidth/6),color*(this.billedHeight/2),1*(this.billedWidth/6),1*(this.billedHeight/2)); //Syntax: Refference image() nederst    
     }
         
