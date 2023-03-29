ba7b3a8db959bb6501051ecdb10be1a9b38f225c
tbdlarsen <105425030+tbdlarsen@users.noreply.github.com>
2023-03-29 13:06:54 +0200

    ændret visual

--- Pieces/King.js
+++ Pieces/King.js
@@ -1,6 +1,7 @@
 class King extends Pawn{
     constructor(x,y){
         super(x,y);
+        this.piece = 0;
 
     }
    
--- Pieces/Knight.js
+++ Pieces/Knight.js
@@ -1,6 +1,7 @@
 class Knight extends Pawn{
     constructor(x,y){
         super(x,y);
+        this.piece = 3;
 
     }
    
