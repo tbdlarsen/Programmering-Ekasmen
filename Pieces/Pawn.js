4d69c5691ae5993c135951a97126ae94c3db04f8
tbdlarsen <105425030+tbdlarsen@users.noreply.github.com>
2023-03-29 13:22:42 +0200

    optimeret pieces

--- Pieces/Pawn.js
+++ Pieces/Pawn.js
@@ -10,15 +10,15 @@ class Pawn {
 
 
         //position af billede og brik
-        this.x = x;
-        this.y = y;
-
+        this.x = x*(width/8);
+        this.y = y*(height/8);
+        this.piece = 5;
     }
 
     //Visual funktion som viser en del af spritesheetet. 
-    visual(color, piece) {    
+    visual(color){    
         image(spritesheet,this.x,this.y,this.billedWidth/6,this.billedHeight/2,
-        piece*(this.billedWidth/6),color*(this.billedHeight/2),1*(this.billedWidth/6),1*(this.billedHeight/2)); //Syntax: Refference image() nederst    
+        this.piece*(this.billedWidth/6),color*(this.billedHeight/2),1*(this.billedWidth/6),1*(this.billedHeight/2)); //Syntax: Refference image() nederst    
     }
         
 }
