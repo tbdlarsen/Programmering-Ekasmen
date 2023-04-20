16537e1d0a5521f25f8d7a1f4f3e8e96cc4fcb34
tbdlarsen <tbdlarsen@gmail.com>
2023-04-20 19:29:56 +0200

    lavet rook movement

--- Pieces/Rook.js
+++ Pieces/Rook.js
@@ -14,25 +14,29 @@ class Rook extends Piece{
 
     Movement(x,x2,y,y2){
 
+      let absX = abs(x2-x);
+      let absY = abs(y2-y);
+
        //1:4 (N)
       if((this.x + this.grid)/this.grid == x2 && (this.y + this.grid)/this.grid == y2 && x2-x == 0 && y2-y >= 1) {
-        this.y += (y2-y)*this.grid; 
+        this.y -= (absY)*this.grid; 
       }
 
       //2:4 (V)
-      if((this.x + this.grid)/this.grid == x2 && (this.y + this.grid)/this.grid == y2 && x2-x == 0 && y2-y >= 1) {
-        this.x -= (x2-x)*this.grid;
+      if((this.x + this.grid)/this.grid == x2 && (this.y + this.grid)/this.grid == y2 && x2-x >= 1 && y2-y == 0) {
+        this.x -= (absX)*this.grid; 
       }
         
       
       //3:4 (Ø)
-      if((this.x + this.grid)/this.grid == x2 && (this.y + this.grid)/this.grid == y2 && x2-x == 0 && y2-y >= 1) {
-        this.x += (x2-x)*this.grid;
+      if((this.x + this.grid)/this.grid == x2 && (this.y + this.grid)/this.grid == y2 && x2-x <= -1 && y2-y == 0) {
+        this.x += (absX)*this.grid;
+        
       }
       
       //4:4 (S)
-      if((this.x + this.grid)/this.grid == x2 && (this.y + this.grid)/this.grid == y2 && x2-x == 0 && y2-y >= 1) {
-        this.y -= (x2-x)*this.grid;
+      if((this.x + this.grid)/this.grid == x2 && (this.y + this.grid)/this.grid == y2 && x2-x == 0 && y2-y <= -1) {
+        this.y += (absY)*this.grid; 
       }
 
 
